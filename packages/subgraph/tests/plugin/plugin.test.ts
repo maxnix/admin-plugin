c4bd5d1578713df6fdbb102a348a6b2116a7a45e
Claudia <claudiabarcelovaldes40@gmail.com>
2024-03-15 10:42:39 +0100

    feat: add tests, constants, and helper functions

--- packages/subgraph/tests/plugin/plugin.test.ts
+++ packages/subgraph/tests/plugin/plugin.test.ts
@@ -1,60 +1,222 @@
-// import {DaoPlugin} from '../../generated/schema';
-// import {handleNumberStored} from '../../src/plugin/plugin';
-// import {CONTRACT_ADDRESS, DAO_ADDRESS} from '../utils/constants';
-// import {createNewNumberStoredEvent} from '../utils/events';
-// import {generatePluginInstallationEntityId} from '@aragon/osx-commons-subgraph';
-// import {Address, DataSourceContext} from '@graphprotocol/graph-ts';
-// import {
-//   assert,
-//   afterEach,
-//   beforeEach,
-//   clearStore,
-//   test,
-//   describe,
-//   dataSourceMock,
-// } from 'matchstick-as';
+import {Action} from '../../generated/schema';
+import {
+  handleProposalExecuted,
+  _handleProposalCreated,
+} from '../../src/plugin/admin';
+import {
+  ADDRESS_ONE,
+  ADDRESS_TWO,
+  STRING_DATA,
+  PLUGIN_PROPOSAL_ID,
+  START_DATE,
+  ALLOW_FAILURE_MAP,
+  CONTRACT_ADDRESS,
+  DAO_ADDRESS,
+} from '../utils/constants';
+import {
+  createNewProposalCreatedEvent,
+  createProposalExecutedEvent,
+  createAdminPluginState,
+  createAdminProposalState,
+} from '../utils/events/plugin';
+import {
+  generateActionEntityId, //   generateDaoEntityId,
+  generatePluginEntityId,
+  generateProposalEntityId,
+  createDummyAction,
+} from '@aragon/osx-commons-subgraph';
+import {
+  Address,
+  Bytes,
+  BigInt,
+  DataSourceContext,
+} from '@graphprotocol/graph-ts';
+import {
+  assert,
+  afterEach,
+  beforeEach,
+  clearStore,
+  test,
+  describe,
+  dataSourceMock,
+} from 'matchstick-as';
 
-// describe('Plugin', () => {
-//   beforeEach(function () {
-//     let context = new DataSourceContext();
-//     context.setString('daoAddress', DAO_ADDRESS);
-//     dataSourceMock.setContext(context);
-//   });
+const actionValue = '0';
+const actionData = '0x00000000';
+const actions = [createDummyAction(ADDRESS_TWO, actionValue, actionData)];
 
-//   afterEach(() => {
-//     clearStore();
-//   });
+const pluginAddress = Address.fromString(CONTRACT_ADDRESS);
+const pluginEntityId = generatePluginEntityId(pluginAddress);
+const pluginProposalId = BigInt.fromString(PLUGIN_PROPOSAL_ID);
+const proposalEntityId = generateProposalEntityId(
+  pluginAddress,
+  pluginProposalId
+);
 
-//   describe('NumberStored Event', () => {
-//     test('it should store the correct number emitted from the event', () => {
-//       const daoAddress = Address.fromString(DAO_ADDRESS);
-//       const pluginAddress = Address.fromString(CONTRACT_ADDRESS);
+describe('Plugin', () => {
+  beforeEach(function () {
+    let context = new DataSourceContext();
+    context.setString('daoAddress', DAO_ADDRESS);
+    dataSourceMock.setContext(context);
+  });
 
-//       const installationId = generatePluginInstallationEntityId(
-//         daoAddress,
-//         pluginAddress
-//       );
-//       if (!installationId) {
-//         throw new Error('Failed to get installationId');
-//       }
-//       // Create state
-//       let daoPlugin = new DaoPlugin(installationId!);
-//       daoPlugin.dao = daoAddress;
-//       daoPlugin.pluginAddress = pluginAddress;
-//       daoPlugin.save();
+  afterEach(() => {
+    clearStore();
+  });
 
-//       const number = '5';
+  describe('handleProposalCreated', () => {
+    test('test the event', () => {
+      // create state
+      createAdminPluginState(pluginEntityId);
 
-//       const event = createNewNumberStoredEvent(
-//         number,
-//         pluginAddress.toHexString()
-//       );
+      // create event
+      let event = createNewProposalCreatedEvent(
+        PLUGIN_PROPOSAL_ID,
+        ADDRESS_ONE,
+        START_DATE,
+        START_DATE,
+        STRING_DATA,
+        actions,
+        ALLOW_FAILURE_MAP,
+        pluginEntityId
+      );
 
-//       handleNumberStored(event);
+      // handle event
+      _handleProposalCreated(event, DAO_ADDRESS, STRING_DATA);
 
-//       assert.fieldEquals('DaoPlugin', installationId!, 'id', installationId!);
-//       assert.fieldEquals('DaoPlugin', installationId!, 'number', number);
-//       assert.entityCount('DaoPlugin', 1);
-//     });
-//   });
-// });
+      let proposalEntityId = generateProposalEntityId(
+        pluginAddress,
+        BigInt.fromString(PLUGIN_PROPOSAL_ID)
+      );
+
+      // checks
+      assert.fieldEquals(
+        'AdminProposal',
+        proposalEntityId,
+        'id',
+        proposalEntityId
+      );
+      assert.fieldEquals('AdminProposal', proposalEntityId, 'dao', DAO_ADDRESS);
+      assert.fieldEquals(
+        'AdminProposal',
+        proposalEntityId,
+        'plugin',
+        pluginEntityId
+      );
+      assert.fieldEquals(
+        'AdminProposal',
+        proposalEntityId,
+        'pluginProposalId',
+        PLUGIN_PROPOSAL_ID
+      );
+      assert.fieldEquals(
+        'AdminProposal',
+        proposalEntityId,
+        'creator',
+        ADDRESS_ONE
+      );
+      assert.fieldEquals(
+        'AdminProposal',
+        proposalEntityId,
+        'metadata',
+        STRING_DATA
+      );
+      assert.fieldEquals(
+        'AdminProposal',
+        proposalEntityId,
+        'executed',
+        'false'
+      );
+      assert.fieldEquals(
+        'AdminProposal',
+        proposalEntityId,
+        'createdAt',
+        event.block.timestamp.toString()
+      );
+      assert.fieldEquals(
+        'AdminProposal',
+        proposalEntityId,
+        'startDate',
+        START_DATE
+      );
+      assert.fieldEquals(
+        'AdminProposal',
+        proposalEntityId,
+        'endDate',
+        START_DATE
+      );
+      assert.fieldEquals(
+        'AdminProposal',
+        proposalEntityId,
+        'allowFailureMap',
+        ALLOW_FAILURE_MAP
+      );
+
+      // check actions
+      for (let index = 0; index < actions.length; index++) {
+        const actionEntityId = generateActionEntityId(proposalEntityId, index);
+        const actionEntity = Action.load(actionEntityId);
+        if (actionEntity) {
+          assert.fieldEquals('Action', actionEntityId, 'id', actionEntityId);
+          assert.fieldEquals('Action', actionEntityId, 'to', ADDRESS_TWO);
+          assert.fieldEquals('Action', actionEntityId, 'value', actionValue);
+          assert.fieldEquals('Action', actionEntityId, 'data', actionData);
+          assert.fieldEquals('Action', actionEntityId, 'dao', DAO_ADDRESS);
+          assert.fieldEquals(
+            'Action',
+            actionEntityId,
+            'proposal',
+            proposalEntityId
+          );
+        }
+      }
+
+      clearStore();
+    });
+  });
+  describe('handleProposalExecuted', () => {
+    test('test the event', () => {
+      // create state
+      createAdminPluginState(pluginEntityId);
+
+      let administratorAddress = Address.fromString(ADDRESS_ONE);
+
+      createAdminProposalState(proposalEntityId, administratorAddress);
+
+      const actionEntityId = generateActionEntityId(proposalEntityId, 0);
+      let action = new Action(actionEntityId);
+      action.to = Address.fromString(ADDRESS_TWO);
+      action.value = BigInt.fromString(actionValue);
+      action.data = Bytes.fromHexString(actionData);
+      action.dao = Address.fromString(DAO_ADDRESS);
+      action.proposal = proposalEntityId;
+      action.save();
+
+      // create event
+      let event = createProposalExecutedEvent(
+        PLUGIN_PROPOSAL_ID,
+        pluginEntityId
+      );
+
+      // handle event
+      handleProposalExecuted(event);
+
+      // checks
+      assert.fieldEquals(
+        'AdminProposal',
+        proposalEntityId,
+        'id',
+        proposalEntityId
+      );
+      assert.fieldEquals('AdminProposal', proposalEntityId, 'executed', 'true');
+      assert.fieldEquals(
+        'AdminProposal',
+        proposalEntityId,
+        'executionTxHash',
+        event.transaction.hash.toHexString()
+      );
+
+      clearStore();
+    });
+  });
+});
--- packages/subgraph/tests/utils/events/plugin.ts
+++ packages/subgraph/tests/utils/events/plugin.ts
@@ -1,22 +1,131 @@
-import {NumberStored} from '../../../generated/templates/Plugin/Plugin';
-import {Address, BigInt, ethereum} from '@graphprotocol/graph-ts';
+import {AdminPlugin, AdminProposal} from '../../../generated/schema';
+import {
+  ProposalCreated,
+  ProposalExecuted,
+} from '../../../generated/templates/Admin/Admin';
+import {
+  ADDRESS_ONE,
+  ADDRESS_TWO,
+  STRING_DATA,
+  PLUGIN_PROPOSAL_ID,
+  START_DATE,
+  ALLOW_FAILURE_MAP,
+  CONTRACT_ADDRESS,
+  DAO_ADDRESS,
+} from '../constants';
+import {Address, BigInt, Bytes, ethereum} from '@graphprotocol/graph-ts';
 import {newMockEvent} from 'matchstick-as';
 
-export function createNewNumberStoredEvent(
-  number: string,
+export function createNewProposalCreatedEvent(
+  proposalId: string,
+  creator: string,
+  startDate: string,
+  endDate: string,
+  metadata: string,
+  actions: ethereum.Tuple[],
+  allowFailureMap: string,
   contractAddress: string
-): NumberStored {
-  let createNumberStoredEvent = changetype<NumberStored>(newMockEvent());
+): ProposalCreated {
+  let createProposalCreatedEvent = changetype<ProposalCreated>(newMockEvent());
 
-  createNumberStoredEvent.address = Address.fromString(contractAddress);
-  createNumberStoredEvent.parameters = [];
+  createProposalCreatedEvent.address = Address.fromString(contractAddress);
+  createProposalCreatedEvent.parameters = [];
 
   let proposalIdParam = new ethereum.EventParam(
-    'number',
-    ethereum.Value.fromSignedBigInt(BigInt.fromString(number))
+    'proposalId',
+    ethereum.Value.fromSignedBigInt(BigInt.fromString(proposalId))
+  );
+  let creatorParam = new ethereum.EventParam(
+    'creator',
+    ethereum.Value.fromAddress(Address.fromString(creator))
+  );
+  let startDateParam = new ethereum.EventParam(
+    'startDate',
+    ethereum.Value.fromSignedBigInt(BigInt.fromString(startDate))
+  );
+  let endDateParam = new ethereum.EventParam(
+    'endDate',
+    ethereum.Value.fromSignedBigInt(BigInt.fromString(endDate))
+  );
+  let metadataParam = new ethereum.EventParam(
+    'metadata',
+    ethereum.Value.fromBytes(Bytes.fromUTF8(metadata))
+  );
+  let actionsParam = new ethereum.EventParam(
+    'actions',
+    ethereum.Value.fromTupleArray(actions)
+  );
+  let allowFailureMapParam = new ethereum.EventParam(
+    'allowFailureMap',
+    ethereum.Value.fromUnsignedBigInt(BigInt.fromString(allowFailureMap))
   );
 
-  createNumberStoredEvent.parameters.push(proposalIdParam);
+  createProposalCreatedEvent.parameters.push(proposalIdParam);
+  createProposalCreatedEvent.parameters.push(creatorParam);
+  createProposalCreatedEvent.parameters.push(startDateParam);
+  createProposalCreatedEvent.parameters.push(endDateParam);
+  createProposalCreatedEvent.parameters.push(metadataParam);
+  createProposalCreatedEvent.parameters.push(actionsParam);
+  createProposalCreatedEvent.parameters.push(allowFailureMapParam);
 
-  return createNumberStoredEvent;
+  return createProposalCreatedEvent;
+}
+
+export function createProposalExecutedEvent(
+  proposalId: string,
+  contractAddress: string
+): ProposalExecuted {
+  let createProposalExecutedEvent = changetype<ProposalExecuted>(
+    newMockEvent()
+  );
+
+  createProposalExecutedEvent.address = Address.fromString(contractAddress);
+  createProposalExecutedEvent.parameters = [];
+
+  let proposalIdParam = new ethereum.EventParam(
+    'proposalId',
+    ethereum.Value.fromSignedBigInt(BigInt.fromString(proposalId))
+  );
+
+  createProposalExecutedEvent.parameters.push(proposalIdParam);
+
+  return createProposalExecutedEvent;
+}
+
+// state
+
+export function createAdminPluginState(
+  pluginEntityId: string,
+  dao: string = DAO_ADDRESS,
+  pluginAddress: string = CONTRACT_ADDRESS
+): AdminPlugin {
+  let adminPlugin = new AdminPlugin(pluginEntityId);
+  adminPlugin.dao = Address.fromString(dao);
+  adminPlugin.pluginAddress = Address.fromString(pluginAddress);
+  adminPlugin.save();
+
+  return adminPlugin;
+}
+
+export function createAdminProposalState(
+  proposalEntityId: string,
+  administratorAddress: Address,
+  daoEntityId: string = DAO_ADDRESS,
+  pluginEntityId: string = CONTRACT_ADDRESS
+): AdminProposal {
+  let adminProposal = new AdminProposal(proposalEntityId);
+  adminProposal.dao = Address.fromString(daoEntityId);
+  adminProposal.plugin = pluginEntityId;
+  adminProposal.pluginProposalId = BigInt.fromString(PLUGIN_PROPOSAL_ID);
+  adminProposal.creator = administratorAddress;
+  adminProposal.metadata = STRING_DATA;
+  adminProposal.executed = false;
+  adminProposal.createdAt = BigInt.fromString(START_DATE);
+  adminProposal.startDate = BigInt.fromString(START_DATE);
+  adminProposal.endDate = BigInt.fromString(START_DATE);
+  adminProposal.allowFailureMap = BigInt.fromString(ALLOW_FAILURE_MAP);
+  adminProposal.administrator = administratorAddress.toHexString();
+  adminProposal.save();
+
+  return adminProposal;
 }
