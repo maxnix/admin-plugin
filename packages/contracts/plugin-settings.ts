ccac7695efb3aeaf6958ee3750273414881d232f
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2025-01-20 17:13:45 +0400

    add permission

--- packages/contracts/plugin-settings.ts
+++ packages/contracts/plugin-settings.ts
@@ -14,7 +14,7 @@ export const PLUGIN_REPO_ENS_SUBDOMAIN_NAME = 'admin'; // 'admin.plugin.dao.eth'
 // For more details, visit https://devs.aragon.org/docs/osx/how-it-works/framework/plugin-management/plugin-repo.
 export const VERSION: VersionTag = {
   release: 1, // Increment this number ONLY if breaking/incompatible changes were made. Updates between releases are NOT possible.
-  build: 1, // Increment this number if non-breaking/compatible changes were made. Updates to newer builds are possible.
+  build: 2, // Increment this number if non-breaking/compatible changes were made. Updates to newer builds are possible.
 };
 
 export const METADATA = {
