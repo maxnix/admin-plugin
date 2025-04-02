934c9993d58021faedbf0a035a8786430de3728e
Jør∂¡ <itsbrickpop@gmail.com>
2025-04-02 12:36:15 +0100

    Update sync-addresses.sh

--- packages/artifacts/sync-addresses.sh
+++ packages/artifacts/sync-addresses.sh
@@ -2,9 +2,6 @@
 
 # Sync the addresses from osx-commons/configs
 
-set -e
-
-
 usage() {
   echo "Usage: $(basename "$0") <source_directory> <destination_file>" >&2
   echo "  <source_directory>: Path to the directory containing the source JSON files." >&2
@@ -20,13 +17,13 @@ fi
 SOURCE_DIR="$1"
 DEST_FILE="$2"
 
-DEPRECATED_NETWORKS=(
+UNSUPPORTED_NETWORKS=(
     "goerli"
     "baseGoerli"
     "devSepolia"
 )
 
-#
+# Checks
 
 if [[ ! -d "$SOURCE_DIR" ]]; then
     echo "Error: Source directory '$SOURCE_DIR' not found." >&2
@@ -43,22 +40,14 @@ if [ "$SOURCE_DIR" == "$(dirname $DEST_FILE)" ]; then
     exit 1
 fi
 
-echo "Processing JSON files in '$SOURCE_DIR'..."
+# Helpers
 
-# Create a temporary file to store intermediate JSON structures
-# Each line in this file will be a JSON object like: {"pluginRepo": {"network_name": "value"}}
-TEMP_MERGE_FILE=$(mktemp)
-
-# Ensure temporary file is removed on script exit (normal or error)
-trap 'rm -f "$TEMP_MERGE_FILE"' EXIT
-
-# Helper functions
 containsElement () {
   local seeking=$1; shift
-  local in=1 # Default is not found (bash false)
+  local in=1 # Not found
   for element; do
     if [[ "$element" == "$seeking" ]]; then
-      in=0 # Found (bash true)
+      in=0 # Found
       break
     fi
   done
@@ -74,15 +63,25 @@ networkAlias () {
     elif [[ "$network" == "zksyncMainnet" ]]; then printf "zksync"
     else printf "$network"
     fi
-
 }
+
+# Ready
+
+echo "Processing $SOURCE_DIR"
+
+# Create a temporary file to store intermediate JSON structures
+# Each line in this file will be a JSON object like: {"pluginRepo": {"network_name": "value"}}
+TEMP_MERGE_FILE=$(mktemp)
+
+# Clean the temp file when the script exits
+trap 'rm -f "$TEMP_MERGE_FILE"' EXIT
 
 # List source address files
 find "$SOURCE_DIR" -maxdepth 1 -name '*.json' | sort | while read source_file; do
     filename=$(basename "$source_file")
     network="${filename%.json}"
 
-    if containsElement "$network" "${DEPRECATED_NETWORKS[@]}"; then
+    if containsElement "$network" "${UNSUPPORTED_NETWORKS[@]}"; then
         echo "Skipping deprecated network: $network"
         continue
     fi
@@ -95,6 +94,7 @@ find "$SOURCE_DIR" -maxdepth 1 -name '*.json' | sort | while read source_file; d
 
     if [[ $jq_exit_code -ne 0 || "$value" == "null" ]]; then
         echo "  Warning: Could not find 'AdminRepoProxy' under 'v1.4.0' or 'v1.3.0' in '$filename'. Skipping." >&2
+        continue
     fi
 
     echo "  Found $value"
@@ -106,11 +106,10 @@ echo "Merging addresses..."
 jq -s 'map(.pluginRepo) | add | {pluginRepo: .}' "$TEMP_MERGE_FILE" > "$DEST_FILE"
 
 
-if [[ $? -eq 0 ]]; then
-    echo "Addresses written to '$DEST_FILE'"
-else
+if [[ $? -ne 0 ]]; then
     echo "Error: Failed to merge the values into '$DEST_FILE'" >&2
     exit 1
 fi
 
+echo "Addresses written to '$DEST_FILE'"
 exit 0
